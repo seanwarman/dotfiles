2c8f865957cb07ca3027e217932a4798713c45e7
Sean <seanwarman@protonmail.com>
2022-06-09 22:03:46 +0100

    General fixes for script's first run

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-export CD3K_CONFIG_DIR=$PWD
+# Make this more general so we could put the install directory anywhere (symlinks?)
+export CD3K_CONFIG_DIR=$HOME/dotfiles
 
-sudo pacman -Syu
-sudo pacman -S w3m xorg-init the_silver_searcher fbgrab
+sudo pacman -S w3m xorg-xinit the_silver_searcher fbgrab
 
 # use a custom .xinitrc
 cp ./X11/.xinitrc $HOME/.xinitrc
-echo ''$CD3K_CONFIG_DIR/cd3k.configrc'' >> $HOME/.zshrc
+echo 'source '$CD3K_CONFIG_DIR/cd3k.configrc'' >> $HOME/.zshrc
 # make vim the default editor (rather than nvim)
 echo 'export EDITOR=/usr/bin/vim' >> $HOME/.zshrc
 echo 'export VISUAL=/usr/bin/vim' >> $HOME/.zshrc
@@ -18,43 +18,44 @@ sudo cat tty/vconsole.conf > /etc/vconsole.conf
 sudo loadkeys /usr/local/share/kbd/keymaps/cd3k.map
 
 # Create ssh key for github
+mkdir $HOME/.ssh
 ssh-keygen -t ed25519 -C "seanwarman@protonmail.com"
-clipcopy < $HOME/.ssh/github.pub
 eval "$(ssh-agent -s)"
 ssh-add $HOME/.ssh/github
+# Can't use clipcopy inside the script for some reason...
+# clipcopy < $HOME/.ssh/github.pub
 
 # Clone my vim/tmux configs
 mkdir $HOME/.vim
-git clone git@github.com:seanwarman/vimrc.git $HOME/.vim
+git clone https://github.com/seanwarman/vimrc.git $HOME/.vim
 touch $HOME/.vimrc
 echo 'runtime vimrc' > $HOME/.vimrc
 touch $HOME/.tmux.conf
 echo 'source-file $HOME/.vim/tmux.conf' > $HOME/.tmux.conf
 
 # Install yay (aur installer)
-sudo git clone https://aur.archlinux.org/yay-git.git $HOME/.config/yay-git
+git clone https://aur.archlinux.org/yay-git.git $HOME/.config/yay-git
 sudo chown -R cd3k:cd3k $HOME/.config/yay-git
 cd $HOME/.config/yay-git
 makepkg -si
 cd $CD3K_CONFIG_DIR
 
 # Install dwm
+# use a custom dwm config file
+cat X11/dwm/config.h > $HOME/.config/dwm/config.h
 git clone git://git.suckless.org/dwm $HOME/.config/dwm
 cd $HOME/.config/dwm
 make
 sudo make install
 cd $CD3K_CONFIG_DIR
-# use a custom dwm config file
-cat $CD3K_CONFIG_DIR/X11/dwm/config.h > $HOME/.config/dwm/config.h
 
 # Install brave
 yay brave
-cd $HOME/.config/BraveSoftware/Brave-Browser/Default
+
 # Sync my bookmarks
-git clone https://github.com/seanwarman/bookmarks.git
-mv bookmarks/* .
+mkdir $HOME/.config/BraveSoftware
+mkdir $HOME/.config/BraveSoftware/Brave-Browser
+mkdir $HOME/.config/BraveSoftware/Brave-Browser/Default
+cd $HOME/.config/BraveSoftware/Brave-Browser/
+git clone https://github.com/seanwarman/bookmarks.git Default
 cd $CD3K_CONFIG_DIR
-
-# TODO tty config files for key mappings...
-
-source $HOME/.zshrc
